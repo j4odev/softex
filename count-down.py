61b1623ba3bc09fe87f1b4676c02c887802a6497
Jão <109635058+j4odev@users.noreply.github.com>
2022-08-02 15:35:35 -0300

    Create count-down.py

---
+++ count-down.py
@@ -0,0 +1,13 @@
+print('-------------------------------')
+print('INICIANDO A CONTAGEM REGRESSIVA')
+print('-------------------------------')
+print('')
+
+import time
+for boom in range(10,0,-1):
+  print(boom)
+  time.sleep(1)
+  
+print('----')
+print('BUM!')
+print('----')
